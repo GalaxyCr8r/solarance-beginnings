f58c204212131f32d15b54bd8f2672ae62cc8fe5
Karl Nyborg <Galaxy613@gmail.com>
2025-05-15 22:59:48 -0400

    Initial stab at STDSL

--- server/src/types/common.rs
+++ server/src/types/common.rs
@@ -1,35 +1,19 @@
-use spacetimedb::{Identity, ReducerContext, SpacetimeType, Table};
+use spacetimedb::ReducerContext;
+use spacetimedsl::dsl;
 
 use super::stellarobjects::player_controlled_stellar_object;
 
 
-
+#[dsl(plural_name = global_configurations)]
 #[spacetimedb::table(name = global_config)]
 pub struct GlobalConfig {
     #[primary_key]
+    #[wrap]
     pub id: u32,
     
     pub active_players: u32,
     pub old_gods_defeated: u8,
 }
-
-#[derive(SpacetimeType, Clone, Debug, PartialEq)]
-pub enum MapView {
-    LocalSpace,
-    LocalSystem,
-    SolarSystem,
-    GalacticSystem
-}
-
-#[spacetimedb::table(name = person, public)]
-pub struct Person {
-    #[primary_key]
-    identity: Identity,
-    name: String,
-    last_view: MapView
-}
-
-//// TODO: Deprecate the below
 
 #[spacetimedb::reducer]
 pub fn try_server_only(ctx: &ReducerContext) -> Result<(), String> {
@@ -64,29 +48,4 @@ pub fn is_server_or_owner(ctx: &ReducerContext, sobj_id: u64) -> Result<(), Stri
         return Ok(());
     }
     Err(IS_SERVER_OR_OWNER_ERROR.to_string())
-}
-
-#[spacetimedb::reducer]
-pub fn add_person(ctx: &ReducerContext, name: String) {
-    ctx.db.person().insert(Person { name, identity: ctx.sender, last_view: MapView::GalacticSystem });
-}
-
-#[spacetimedb::reducer]
-pub fn say_hello(ctx: &ReducerContext) {
-    for person in ctx.db.person().iter() {
-        log::info!("Hello, {}!", person.name);
-    }
-    log::info!("Hello, World!");
-}
-
-#[spacetimedb::reducer]
-pub fn set_map_view(ctx: &ReducerContext, new_view: MapView) -> Result<(), String> {
-    
-    if let Some(user) = ctx.db.person().identity().find(ctx.sender) {
-        ctx.db.person().identity().update(Person { last_view: new_view, ..user });
-        log::info!("New view set!");
-        Ok(())
-    } else {
-        Err("Cannot set name for unknown user".to_string())
-    }
 }
