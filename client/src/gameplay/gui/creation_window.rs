2252a15b1d50f7337ce1ea056db17458716ca9c0
Karl Nyborg <karl.nyborg@blackcape.io>
2025-08-15 15:55:44 -0400

    Removed debug info from user/ship creation screen.

--- client/src/gameplay/gui/creation_window.rs
+++ client/src/gameplay/gui/creation_window.rs
@@ -1,7 +1,7 @@
-use egui::{ Align2, Color32, Context, RichText };
-use spacetimedb_sdk::{ DbContext, Table };
+use egui::{Align2, Color32, Context, RichText};
+use spacetimedb_sdk::{DbContext, Table};
 
-use crate::{ gameplay::state::GameState, module_bindings::*, stdb::utils::* };
+use crate::{gameplay::state::GameState, module_bindings::*, stdb::utils::*};
 
 // #[derive(PartialEq)]
 // enum CurrentTab {
@@ -26,10 +26,9 @@ impl State {
 pub fn draw(
     egui_ctx: &Context,
     ctx: &DbConnection,
-    game_state: &mut GameState
+    game_state: &mut GameState,
 ) -> Option<egui::InnerResponse<Option<()>>> {
-    egui::Window
-        ::new("Account Creation")
+    egui::Window::new("Account Creation")
         .title_bar(true)
         .resizable(false)
         .collapsible(false)
@@ -40,39 +39,25 @@ pub fn draw(
             ui.vertical_centered(|ui| {
                 if let Some(player) = get_player(&ctx.db, &ctx.identity()) {
                     create_ship(ctx, game_state, ui, player);
-                } else if let Some(player_ship) = get_player_ship(&ctx) {
+                } else if let Some(_player_ship) = get_player_ship(&ctx) {
                     // You're ready to go!
                 } else {
                     create_player(ctx, game_state, ui);
                 }
                 if game_state.creation_window.error.is_some() {
                     ui.label(
-                        RichText::new(
-                            format!(
-                                "ERROR: {}",
-                                game_state.creation_window.error.as_ref().unwrap().to_string()
-                            )
-                        )
-                            .strong()
-                            .color(Color32::RED)
+                        RichText::new(format!(
+                            "ERROR: {}",
+                            game_state
+                                .creation_window
+                                .error
+                                .as_ref()
+                                .unwrap()
+                                .to_string()
+                        ))
+                        .strong()
+                        .color(Color32::RED),
                     );
-                }
-                ui.separator();
-                for player in ctx.db().player().iter() {
-                    ui.label(RichText::new(format!("{}: {}", player.username, player.id)));
-                }
-                for station in ctx.db().station().iter() {
-                    ui.label(
-                        RichText::new(
-                            format!("{}:  {} - {}", station.name, station.id, station.sobj_id)
-                        )
-                    );
-                    if let Some(sobj) = ctx.db().stellar_object().id().find(&station.sobj_id) {
-                        ui.label(RichText::new(format!("   SOBJ Kind: {:?}", sobj.kind)));
-                    }
-                }
-                for sector in ctx.db().sector().iter() {
-                    ui.label(RichText::new(format!("{}:  {}", sector.name, sector.id)));
                 }
                 ui.separator();
                 if ui.button("Exit").clicked() {
@@ -86,7 +71,7 @@ fn create_ship(
     ctx: &DbConnection,
     game_state: &mut GameState<'_>,
     ui: &mut egui::Ui,
-    player: Player
+    player: Player,
 ) {
     // Create a ship
     ui.heading(format!("Welcome Captain {}!", player.username));
@@ -98,7 +83,7 @@ fn create_ship(
     ui.separator();
     ui.heading("Basic Instructions");
     ui.label(
-        "As of 0.2.x, you can only mine asteroids and travel to different sectors via jump gates. There is no combat or NPC ships yet."
+        "Currently you can only mine asteroids and travel to different sectors via jump gates. There is no combat or NPC ships yet."
     );
     ui.label(
         "The next milestone will bring space stations where you'll be able to sell ore at and perhaps other things!"
@@ -108,15 +93,16 @@ fn create_ship(
         "To use jump gates, engage auto-docking with its hotkey and get to its exact center. Wait for it to drain all your energy."
     );
     ui.label(
-        "To mine asteroids, target a asteroid and use mining on it. The hotkeys should be obvious via the GUI."
+        "To mine asteroids, target a asteroid with '[E]' or the minimap and use '[X]' mining on it. The hotkeys should be obvious via the GUI."
     );
     ui.separator();
-    if ui.button(RichText::new("Create Your Ship").strong()).clicked() {
-        match
-            ctx.reducers.create_player_controlled_ship(
-                ctx.identity(),
-                game_state.chat_window.text.clone()
-            )
+    if ui
+        .button(RichText::new("Create Your Ship").strong())
+        .clicked()
+    {
+        match ctx
+            .reducers
+            .create_player_controlled_ship(ctx.identity(), game_state.chat_window.text.clone())
         {
             Ok(_) => {
                 game_state.creation_window.error = None;
@@ -141,11 +127,9 @@ fn create_player(ctx: &DbConnection, game_state: &mut GameState<'_>, ui: &mut eg
         if ui.button("Create").clicked() || ui.input(|i| i.key_pressed(egui::Key::Enter)) {
             if !game_state.creation_window.text.is_empty() {
                 // Create Player
-                match
-                    ctx.reducers.register_playername(
-                        ctx.identity(),
-                        game_state.creation_window.text.clone()
-                    )
+                match ctx
+                    .reducers
+                    .register_playername(ctx.identity(), game_state.creation_window.text.clone())
                 {
                     Ok(_) => {
                         game_state.creation_window.error = None;
